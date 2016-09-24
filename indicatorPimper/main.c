c1ec641ccdb4f29d8c0a26b0da84d4e138106897
Salvatore Tropea <tropea05@yahoo.de>
2016-09-24 14:24:31 +0200

    add main.c FIle with empty functionality

---
+++ indicatorPimper/main.c
@@ -0,0 +1,19 @@
+/*
+ * indicatorPimper.c
+ *
+ * Start Development:	24.09.2016 14:13:49
+ * Author :		Tropea Salvatore
+ * Version:		V a1.00.00
+ * Description:		Application to modify the flashing mode of Indicator Lamps in cars.
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
