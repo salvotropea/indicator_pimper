673d4676f6e9bb8df442a53aa0270ad958b19d98
Salvatore Tropea <tropea05@yahoo.de>
2016-09-29 22:07:20 +0200

    add Header File to define Hardware to encouple Scource Code

---
+++ indicatorPimper/HardwareDef.h
@@ -0,0 +1,16 @@
+/*
+ * HardwareDef.h
+ *
+ * Created: 29.09.2016 22:00:31
+ *  Author: Tropea
+ */ 
+
+
+#ifndef HARDWAREDEF_H_
+#define HARDWAREDEF_H_
+#define output PORTL
+#define input PINK
+#define F_CPU 16000000UL
+
+
+#endif /* HARDWAREDEF_H_ */
