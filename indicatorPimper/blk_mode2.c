35e3bde9af082b506c36f2218993c22cb45d62a1
Salvatore Tropea <tropea05@yahoo.de>
2016-10-08 15:02:18 +0200

    add extra File for dimm mode

---
+++ indicatorPimper/blk_mode2.c
@@ -0,0 +1,54 @@
+/*
+ * blk_mode1.c
+ *
+ * Start Development:	24.09.2016 14:13:49
+ * Author :		Tropea Salvatore
+ * Version:		V a1.00.00
+ * Description:		Application to modify the flashing mode of Indicator Lamps in cars.
+ *			Blinking_mode 1 runs a ProgressBar with 8 LEDs
+ */
+
+#include "HardwareDef.h"
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+//Dimm LEDs on and off (Ramp)
+int dutyCycle = 0;
+int cycles = 0;
+void blk_mode2(){
+	TCCR5A = (1 << COM5A1) | (1 << WGM00) | (1 << WGM01);
+	TIMSK5 = (1 << TOIE5);
+	
+	OCR5AL = dutyCycle;
+	//OCR5AH = ;
+	
+	sei();
+	
+	TCCR5B = (1 << CS00);
+	
+	OUTPUT = 0x01;
+	
+	while (1)
+	{
+		
+	}
+	
+}
+
+ISR(TIMER5_OVF_vect)
+{
+	if (cycles < 150)
+	{
+		cycles++;
+	} 
+	else
+	{
+		dutyCycle = dutyCycle + 5;
+		cycles = 0;
+	}
+	
+	OCR5AL = dutyCycle;
+}
