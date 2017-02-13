d676ac7a947843f0c8b637b6d714f10ad3a37ef2
Salvatore Tropea <tropea05@yahoo.de>
2017-02-13 19:06:33 +0100

    Dimmfunktion auf atm16 angepasst, läuft jetzt besser denn je, das timing muss noch angepasst werden, es tut aber was es soll.

--- indicatorPimper/blk_mode2.c
+++ indicatorPimper/blk_mode2.c
@@ -20,19 +20,22 @@ int dutyCycle = 0;		    //PWM Value 0-0xff Comparisionvalue
 int cycles = 0;			    //Interval in prog. cycles for slow dimm up and down
 char direction = 0;		    //Direction of Dimm. 0->up 1->down
 char state = 0;			    //State of function
-unsigned int interval = 120;	    //interval value (for cycles) default-> ??
+unsigned int interval = 1000;	    //interval value (for cycles) default-> ??
 unsigned int speed = 5;		    //Speed up value (for dutyCylce increment or decrement) default-> ??
 
 void blk_mode2(){
-	TCCR0 = (1 << COM01) | (1 << COM00) | (1 << WGM00) | (1 << WGM01) | (1 << WGM00) ;
+	TCCR0 = (1 << WGM00) | (1 << COM01) | (1 << COM00) | (1 << WGM01) | (1 << CS00);
+	
 	TIMSK = (1 << TOIE0);
+	TIFR = (1 << TOV0);
 	
 	OCR0 = dutyCycle;
 	
+	
+	
 	state = 1;
 	sei();
-	TCCR0 = (1 << CS00);
-	OUTPUT = 0x00;
+	//OUTPUT = 0x00;
 }
 
 //-Interrupt Service Routine for TIM5 Overflow
@@ -49,20 +52,20 @@ ISR(TIMER0_OVF_vect)
 			switch (direction) //Dimm up or down
 			{
 				case 0: //Dimm up and revert direction when reached max
-				dutyCycle = dutyCycle + speed;
-				if (dutyCycle >= 255)
+				dutyCycle = dutyCycle - speed;
+				if (dutyCycle <= 0)
 				{
 					direction = 1;
 				}
 				break;
 			
 				case 1: //Dimm down and revert direction when reached min and end ISR
-				dutyCycle = dutyCycle - speed;
-				if (dutyCycle <= 0)
+				dutyCycle = dutyCycle + speed;
+				if (dutyCycle > 254)
 				{
 					direction = 0;
 					state = 0;
-					dutyCycle = 0;
+					dutyCycle = 255;
 					cli();
 				
 				}
