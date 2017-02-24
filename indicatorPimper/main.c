5670ab750d1a27e3fbf66a6e33ea8e326b9e67c0
Salvatore Tropea <tropea05@yahoo.de>
2017-02-24 23:28:09 +0100

    TestCommit vom LapTop

--- indicatorPimper/main.c
+++ indicatorPimper/main.c
@@ -1,9 +1,9 @@
 /*
  * indicatorPimper.c
  *
- * Start Development:	24.09.2016 14:13:49
+ * Start Development:	01.03.2017 14:13:49
  * Author :				Tropea Salvatore
- * Version:				V a1.00.00
+ * Version:				V c1.00.00
  * Description:			Application to modify the flashing mode of Indicator Lamps in cars.
  */ 
 
